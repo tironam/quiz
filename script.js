db9e213fc9f0691b29bbc879e5bf2c2849d85634
Michael Tirona <tironamichael@gmail.com>
2020-04-29 22:02:52 -0700

    Added question objects/arrays

---
+++ script.js
@@ -0,0 +1,58 @@
+let all_questions = [
+    {
+        question_string: `What show does the term "hotdog" come from?`,
+        choices: {
+            correct: 'Silicon Valley',
+            wrong: ['The Big Bang Theory', 'Mr. Robot', 'Freaks & Geeks']
+        }
+    },
+    {
+        question_string: `Temperate Literals use:`,
+        choices: {
+            correct: 'Backticks',
+            wrong: ['Curly Braces', 'Single Quotes', 'Double Quotes']
+        }
+    },
+    {
+        question_string: `A Boolean is a True/False statement?`,
+        choices: {
+            correct: 'True',
+            wrong: ['False']
+        }
+    },
+    {
+        question_string: `What popular styling library was made by Twitter?`,
+        choices: {
+            correct: 'Bootstrap',
+            wrong: ['JQuery', 'Box Lunch', 'Pied Piper']
+        }
+    },
+    {
+        question_string: `W3 Schools is a:`,
+        choices: {
+            correct: 'Great coding resource',
+            wrong: ['A styling library', 'Art school', 'Middle-out compression company']
+        }
+    },
+    {
+        question_string: `It's best practice to use _____ for CSS`,
+        choices: {
+            correct: 'Classes'
+            wrong: ['<a> links', 'IDs', 'Scripts']
+        }
+    },
+    {
+        question_string: `The best browser for a web developer to use is:`,
+        choices: {
+            correct: 'Google Chrome',
+            wrong: ['Internet Explorer', 'Safari', 'Firefox'']
+        }
+    },
+    {
+        question_string: `A great place to store your code repositories is:`,
+        choices: {
+            correct: 'GitHub'
+            wrong: ['An incubator', 'Raviga', 'Hooli']
+        }
+    },
+]
